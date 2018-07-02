43c4d51678e90821b4a1753e41e17e0833b30f68
facoulon <f.coulon48@laposte.net>
2018-07-02 10:41:23 +0200

    5 caracteres verifications + small change

--- js/app.js
+++ js/app.js
@@ -1,17 +1,41 @@
-$(document).ready(function(){
-  $("#pseudo-input").focusout(function(){
+$(document).ready(function() {
+  var toto = "";
+
+  $(".form-control").focusout(function() {
+    if ($(this).attr("id") == "pseudo-input") {
+      toto = "nickname";
+    } else if ($(this).attr("id") == "exampleInputPassword1" || $(this).attr("id") == "exampleInputPassword2") {
+      toto = "password";
+    } else {
+      toto = "email";
+    }
     $(this).val();
-    if ($(this).val().length < 6 ) {
+    if ($(this).val().length < 5 && $(this).val().length > 0) {
       console.log("short");
       $(this).addClass("border-warning");
-      $("#pseudoHelp").text("nickname too short, need 5 character");
+      $(this).removeClass("border-success");
+      $(this).next().text(toto + "too short, need 5 characters");
+    } else if ($(this).val().length == 0) {
+      $(this).removeClass("border-success");
+      $(this).removeClass("border-warning");
+      $(this).next().text("Please type your" + toto);
+    } else {
+      $(this).addClass("border-success");
+      $(this).removeClass("border-warning");
+      $(this).next().text(toto + " valid");
     }
   })
+  $(".form-control").focusin(function() {
+    $(this).removeClass("border-success");
+    $(this).removeClass("border-warning");
+    $(this).next().text("Need at least 5 characters");
+  });
 
-
-
-
-
+$("#exampleInputPassword2").focusout(function() {
+  if ($(this).val() == $("#exampleInputPassword1").val()) {
+    console.log("victoire");
+  }
+});
 
 
 
