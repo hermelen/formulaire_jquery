4531ad06ed256706f3f3f62918eed91e787698d1
hermelen <Hermelen>
2018-07-02 09:51:08 +0200

    html css done beginning jquery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-  console.log('hello world');
+  $("#pseudo-input").focusout(function(){
+    $(this).val();
+    if ($(this).val().length < 6 ) {
+      console.log("short");
+      $(this).addClass("border-warning");
+      $("#pseudoHelp").text("nickname too short, need 5 character");
+    }
+  })
+
+
+
+
+
+
+
+
+
+
 })
